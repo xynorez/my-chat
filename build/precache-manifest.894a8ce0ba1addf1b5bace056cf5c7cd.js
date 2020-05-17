da445e2912a9cc44d66e050a0e8891a08f4dc3a5
xynorez <leschmarci@gmail.com>
2020-05-17 14:30:50 +0200

    build added

---
+++ build/precache-manifest.894a8ce0ba1addf1b5bace056cf5c7cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e432387d107c5b48816df30f2407fa2d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3909c7ffe93363334bc3",
+    "url": "./static/css/main.00c86594.chunk.css"
+  },
+  {
+    "revision": "5ed28e986c9d3ccfaaa3",
+    "url": "./static/js/2.0f4066fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0f4066fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3909c7ffe93363334bc3",
+    "url": "./static/js/main.392898cb.chunk.js"
+  },
+  {
+    "revision": "3b5176513c78f0fdd135",
+    "url": "./static/js/runtime-main.03defe92.js"
+  }
+]);
