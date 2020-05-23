3733fbd84a7dd2c1d46aef44eb5b4e6ebeacbd01
xynorez <leschmarci@gmail.com>
2020-05-23 16:45:11 +0200

    additional fixes

--- src/Login.tsx
+++ src/Login.tsx
@@ -12,15 +12,15 @@ export class Login extends Component
         <img src="logo512.png" width="256" />
         <TextInput type="email" placeholder="Email (someone@example.com)" value={this.state.email} onChange={e => this.setName(e)} onEnter={() => this.onClick()} autofocus={true} />
         <TextInput type="password" placeholder="Password" value={this.state.password} onChange={e => this.setState({ password: e })} onEnter={() => this.onClick()} autofocus={true} />
-        
-        <button type="button" onClick={ () => this.onClick() }>
-        { this.state.register ? "Register" : "Login" }
-        </button>
 
         { this.state.register &&
             <TextInput type="text" placeholder="Display Name (Agent Smith)" value={ this.state.displayName }
             onChange={ e => this.setState( { displayName: e } ) } /> 
         }
+
+        <button type="button" onClick={ () => this.onClick() }>
+        { this.state.register ? "Register" : "Login" }
+        </button>
 
         <p>{this.state.register ? "Switch back to " : "Have no account yet? Go and "}
             <a href="" onClick={e => { e.preventDefault(); this.setState({ register: !this.state.register }); }}>
@@ -43,8 +43,8 @@ export class Login extends Component
     }
 
     setName(e: string){
-        if (this.state.register === false) {
-            if (e === "JRMP4U") {
+        if (this.state.register) {
+            if (e === "JRMP4U" || e === "jrmp4u") {
                 this.setState({ email: e, displayName: "Marcell" });
             }
         }
